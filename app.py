3c668664bfd00f32565508bcfd7f415a0f19e392
BA Aliou <alioubafr@gmail.com>
2018-06-27 19:53:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello Dakar Inno Day V3. "+ socket.gethostname()
+    return "<h1>Hello Dakar Inno Day V3.</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=80)
