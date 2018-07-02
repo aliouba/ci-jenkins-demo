211546f80c0300c24dbc66c830574eb0e78c874c
root <root@instance.c.izibill-186500.internal>
2018-07-02 13:06:42 +0000

    V146

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "<h1><center>Hello Dakar Innovation Day.</center></h1>"
+    return "<h1><center>Hello Dakar Innovation Day V146.</center></h1>"
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=80)
